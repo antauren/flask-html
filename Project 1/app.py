6815bcf08c2bc6060a27c3e5f307d364eaa2c481
Anatoly <antauren@yandex.ru>
2020-05-04 21:06:32 +0300

    bonus step1

--- Project 1/app.py
+++ Project 1/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
+from data import tours
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', tours=tours.values())
 
 
 @app.route('/departures/<departure>/')
@@ -18,4 +20,4 @@ def render_tour(id):
     return render_template('tour.html')
 
 
-app.run()
+app.run(debug=True)

f74a4c8ae182b0eb6ed9e893bc05e98a5a67859d
Anatoly <antauren@yandex.ru>
2020-05-05 14:40:30 +0300

    fix view indexes

--- Project 1/app.py
+++ Project 1/app.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 
 from data import tours
 
+tours_dict = tours.copy()
+for id_, tour in tours_dict.items():
+    tours_dict['id'] = id_
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('index.html', tours=tours.values())
+    return render_template('index.html', tours=tours_dict.values())
 
 
 @app.route('/departures/<departure>/')
